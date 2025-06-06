cdbe33b7288870b55faf6ce80f520f117bde0858
Anders Reenberg Andersen <andersra@live.dk>
2025-06-07 01:06:09 +0200

    Update test.py

--- src/test.py
+++ src/test.py
@@ -9,16 +9,16 @@ import numpy as np
 sample_size = 100
 obs = np.zeros(sample_size)
 #continuous
-#d = ph.dist(discrete=False,initdist=np.array([1.0,0.0,0.0]),phgen=np.matrix([[-2.0,2.0,0.0],[0.0,-2.0,2.0],[0.0,0.0,-2.0]]))
-#for i in range(sample_size):
-#    obs[i] = d.getsample()
-#discrete
-d = ph.dist(discrete=True,initdist=np.array([0.5,0.0,0.5]),phgen=np.matrix([[0.5,0.5,0.0],[0.0,0.5,0.5],[0.0,0.0,0.5]]))
+d = ph.dist(discrete=False,initdist=np.array([1.0,0.0,0.0]),phgen=np.matrix([[-2.0,2.0,0.0],[0.0,-2.0,2.0],[0.0,0.0,-2.0]]))
 for i in range(sample_size):
     obs[i] = d.getsample()
+#discrete
+#d = ph.dist(discrete=True,initdist=np.array([0.5,0.0,0.5]),phgen=np.matrix([[0.5,0.5,0.0],[0.0,0.5,0.5],[0.0,0.0,0.5]]))
+#for i in range(sample_size):
+#    obs[i] = d.getsample()
    
 #fit the distribution
-fit = ph.fit(obs=obs,nphases=3,dtype="gencoxian",discrete=True,verbose=True)
+fit = ph.fit(obs=obs,nphases=3,dtype="generlang",discrete=False,verbose=True)
 
 #check the fitted parameters
 print(fit.getinitdist())
