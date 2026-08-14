24a5f60925771c52f3627068436aa4a0a279898c
areenberg <andersra@live.dk>
2026-08-15 00:32:08 +0200

    Prepared for implementing censored observations

--- src/phasedist/ecph.py
+++ src/phasedist/ecph.py
@@ -24,6 +24,10 @@ class ecph:
         """
         
         self.nphases=nphases
+        self.bi = np.zeros(self.nphases)
+        self.zi = np.zeros(self.nphases)
+        self.ni = np.zeros(self.nphases)
+        self.nij = np.zeros((self.nphases, self.nphases))
 
         return None
 
@@ -50,21 +54,91 @@ class ecph:
         self.eTy = mat[: self.nphases, : self.nphases]
         self.Jmat = mat[: self.nphases, self.nphases : 2 * self.nphases]
 
+    def __uncensored(self,y):
+        """
+        Updates b_i, z_i, n_i, n_ij with the contribution
+        from a single fully observed (uncensored) observation y.
+
+        Args:
+            y (float): Observation value.
+
+        Returns:
+            None
+        """
+
+        self.__Jmatrix(y)
+
+        eTyt = np.matmul(self.eTy, self.exitrates)
+        pieTy = np.matmul(self.initdist, self.eTy)
+        self.pieTyt = np.matmul(pieTy, self.exitrates)
+
+        for i in range(self.nphases):
+            self.bi[i] += (self.initdist[i] * eTyt[i]) / self.pieTyt
+            self.zi[i] += self.Jmat[i, i] / self.pieTyt
+            for j in range(self.nphases):
+                if j != i:
+                    self.nij[i, j] += (self.phgen[i, j] * self.Jmat[j, i]) / self.pieTyt
+            self.ni[i] += (pieTy[i] * self.exitrates[i]) / self.pieTyt
+
+    def __rightcensored(self,right):
+        # CODE HERE
+        return 0
+
+    def __leftcensored(self,left):
+        # CODE HERE
+        return 0
+
+    def __intervalcensored(self,left,right):
+        # CODE HERE
+        return 0
+
+    def __storefundamental(
+                            self,
+                            initdist: np.array,
+                            phgen: np.array,
+                            exitrates: np.array
+                        ):
+        """
+        Stores the CPH distribution's fundamental parameters as instance attributes so they
+        can be accessed by the other methods during the E-step calculations.
+
+        Args:
+            initdist (ndarray): Initial distribution vector.
+            phgen (ndarray): Phase-type generator.
+            exitrates (ndarray): Exit-rate vector.
+
+        Returns:
+            None
+        """
+
+        self.initdist = initdist
+        self.phgen = phgen 
+        self.exitrates = exitrates
+
     def run(
             self,
             obs: np.array,
             initdist: np.array,
             phgen: np.array,
-            exitrates: np.array
+            exitrates: np.array,
+            censoring: np.array = None
         ):
         """
         Performs the calculations of the E-step.
+
+        Censored observations can be specified using the (n_obs x 2) censoring ndarray where each
+        row corresponds to an observation and the two columns determines if an observation is censored:
+        - [np.nan,np.nan] -> uncensored.
+        - [np.nan,float] -> right-censored.
+        - [float,np.nan] -> left-censored.
+        - [float,float] -> interval-censored.
 
         Args:
             obs (ndarray): Array of observations.
             initdist (ndarray): Initial distribution vector.
             phgen (ndarray): Phase-type generator.
             exitrates (ndarray): Exit-rate vector.
+            censoring (ndarray): Specifies censored observations.
 
         Returns:
             ndarray: Number of processes that initiated in state i (b_i).
@@ -72,29 +146,33 @@ class ecph:
             ndarray: Number of processes that exited to the absorbing state from state i (n_i).
             ndarray: Number of processes that jumped from state i to state j (n_ij).
         """
-        self.initdist = initdist
-        self.phgen = phgen 
-        self.exitrates = exitrates
-        self.bi = np.zeros(self.nphases)
-        self.zi = np.zeros(self.nphases)
-        self.ni = np.zeros(self.nphases)
-        self.nij = np.zeros((self.nphases, self.nphases))
+
+        self.__storefundamental(initdist,phgen,exitrates)
+
+        self.bi.fill(0)
+        self.zi.fill(0)
+        self.ni.fill(0)
+        self.nij.fill(0)
 
         self.loglikelihood = 0.0
-        
-        for y in obs:
-            self.__Jmatrix(y)
-            eTyt = np.matmul(self.eTy, self.exitrates)
-            pieTy = np.matmul(self.initdist, self.eTy)
-            pieTyt = np.matmul(pieTy, self.exitrates)
-            self.loglikelihood += np.log(pieTyt)
-            for i in range(self.nphases):
-                self.bi[i] += (self.initdist[i] * eTyt[i]) / pieTyt
-                self.zi[i] += self.Jmat[i, i] / pieTyt
-                for j in range(self.nphases):
-                    if j != i:
-                        self.nij[i, j] += (self.phgen[i, j] * self.Jmat[j, i]) / pieTyt
-                self.ni[i] += (pieTy[i] * self.exitrates[i]) / pieTyt
 
+        if censoring is not None:
+            for idx,y in enumerate(obs):
+
+                if np.isnan(censoring[idx,0]) and np.isnan(censoring[idx,1]):
+                    self.__uncensored(y) #uncensored observation
+                elif np.isnan(censoring[idx,0]) and not np.isnan(censoring[idx,1]):
+                    self.__rightcensored(censoring[idx,1]) #Right-censored observation
+                elif not np.isnan(censoring[idx,0]) and np.isnan(censoring[idx,1]):
+                    self.__leftcensored(censoring[idx,0]) #Left-censored observation
+                elif not np.isnan(censoring[idx,0]) and not np.isnan(censoring[idx,1]):
+                    self.__intervalcensored(censoring[idx,0],censoring[idx,1]) #Interval-censored observation
+
+                self.loglikelihood += np.log(self.pieTyt) #<<<--- check if needs to be dependent on censoring type    
+
+        else:
+            for y in obs:
+                self.__uncensored(y)
+                self.loglikelihood += np.log(self.pieTyt)
 
         return self.bi,self.zi,self.ni,self.nij
